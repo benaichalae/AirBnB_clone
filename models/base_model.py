9ddad71ded5b0b853a25bf45ab755a575bffd68e
benaichalae <benaichalae@gmail.com>
2023-11-07 18:50:07 -0800

    hbnb

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import models
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name = key.split('.')[0]
+                    obj = BaseModel(**value)
+                    self.new(obj)
+        except FileNotFoundError:
+            pass
+
