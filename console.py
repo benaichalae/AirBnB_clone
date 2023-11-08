f941e78f6f24236a5a68a512475c4c418dcf0429
benaichalae <benaichalae@gmail.com>
2023-11-08 08:30:51 -0800

    hbnb

--- console.py
+++ console.py
@@ -4,11 +4,17 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    valid_classes = ["BaseModel"]  # Add more class names as needed
+    valid_classes = ["BaseModel", "User", "State", "City", "Place", "Amenity", "Review"]
 
     def emptyline(self):
         """Do nothing on an empty line (pressing ENTER)"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,15 +18,32 @@ class FileStorage:
         obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(obj_dict, file)
-
+    
     def reload(self):
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
-                    class_name = key.split('.')[0]
-                    obj = BaseModel(**value)
+                    class_name = value.get('__class__')
+                    if class_name == 'BaseModel':
+                        obj = BaseModel(**value)
+                    elif class_name == 'User':
+                        obj = User(**value)
+                    else:
+                        continue
                     self.new(obj)
         except FileNotFoundError:
             pass
-
